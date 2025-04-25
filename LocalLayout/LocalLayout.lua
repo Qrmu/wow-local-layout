d032cea2f25aaba84a3fba1b51ff5c011d08aa06
Matti Mykkänen <matti.mykkanen@infrakit.com>
2025-04-25 23:02:48 +0300

    Hardcode the layout to use

--- LocalLayout/LocalLayout.lua
+++ LocalLayout/LocalLayout.lua
@@ -3,18 +3,11 @@ LocalLayoutHelper:RegisterEvent ("PLAYER_ENTERING_WORLD")
 LocalLayoutHelper:RegisterEvent ("PLAYER_SPECIALIZATION_CHANGED")
 
 LocalLayoutHelper:SetScript ("OnEvent", function (self, event, isLogin, isReload)
-    if LocalLayoutDB and LocalLayoutDB.activeLayout then
-        C_EditMode.SetActiveLayout(LocalLayoutDB.activeLayout)
-    else
-        print("LocalLayout: No Edit Mode layout saved. Apply a layout in Edit Mode to save it for this character.")
-    end
+    if ((event == "PLAYER_ENTERING_WORLD" and (isLogin or isReload))
+            or event == "PLAYER_SPECIALIZATION_CHANGED") then
+        local activeLayout = 4
 
-    if (event == "PLAYER_ENTERING_WORLD") then
-        if isLogin or isReload then
-            hooksecurefunc(C_EditMode, "SetActiveLayout", function()
-                local layoutInfo = C_EditMode.GetLayouts()
-                LocalLayoutDB = { activeLayout = layoutInfo.activeLayout }
-            end)
-        end
+        C_EditMode.SetActiveLayout(activeLayout)
+        print("LocalLayout: Set Active Layout " .. activeLayout)
     end
 end)
