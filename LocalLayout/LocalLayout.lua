c5c5f3a38afa9922e56f2710f0a55019411a80f7
Matti Mykkänen <matti.mykkanen@infrakit.com>
2025-04-26 15:10:17 +0300

    ActiveLayout should not be set when other players change their spec.

--- LocalLayout/LocalLayout.lua
+++ LocalLayout/LocalLayout.lua
@@ -2,12 +2,11 @@ local LocalLayoutHelper = CreateFrame ("frame", "LocalLayoutHelper", UIParent)
 LocalLayoutHelper:RegisterEvent ("PLAYER_ENTERING_WORLD")
 LocalLayoutHelper:RegisterEvent ("PLAYER_SPECIALIZATION_CHANGED")
 
-LocalLayoutHelper:SetScript ("OnEvent", function (self, event, isLogin, isReload)
-    if ((event == "PLAYER_ENTERING_WORLD" and (isLogin or isReload))
-            or event == "PLAYER_SPECIALIZATION_CHANGED") then
+LocalLayoutHelper:SetScript ("OnEvent", function (self, event, p1, p2)
+    if ((event == "PLAYER_ENTERING_WORLD" and (p1 or p2))
+            or (event == "PLAYER_SPECIALIZATION_CHANGED" and p1 == "player")) then
         local activeLayout = 4
 
         C_EditMode.SetActiveLayout(activeLayout)
-        print("LocalLayout: Set Active Layout " .. activeLayout)
     end
 end)
